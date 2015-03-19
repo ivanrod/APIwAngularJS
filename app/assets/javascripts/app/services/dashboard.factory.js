af26c1d8d2289d95d615cc498d5dd634a41ae062
Ivan Rodriguez <frivanrodriguez@gmail.com>
2015-03-19 23:53:08 +0100

    Added comments with Jdocs

--- app/assets/javascripts/app/services/dashboard.factory.js
+++ app/assets/javascripts/app/services/dashboard.factory.js
@@ -1,10 +1,17 @@
-/* Dashboard Helper Function Factory */
-
+/**
+ * Dashboard Helper Function Factory
+ * @namespace dashboardFactory
+ * @memberOf Factories
+ */
 
 /* CAMBIAR NOMBRE POR CHARTFACTORY!!!!! */
 function dashboardFactory(sharedData){
   'use strict';
   return {
+    /**
+     * @desc Obtain an array with the last 7 days     
+     * @return {Array}  Elements are formated: dd/mm/yy
+     */   
     last7daysArray: function(){
         var last7daysArray = [];
         var day = new Date(Date.now())
@@ -16,9 +23,13 @@ function dashboardFactory(sharedData){
         }
         return last7daysArray.reverse()
     },
+    /**
+     * Obtains the last 7 days alerts for an user  
+     * @param  {Object} user 
+     * @return {Array} 7 elements (sorted by 7 days)
+     */
     lastAlertsUserNum: function(user){
-      //7 zeros array
-      var lastAlerts = Array.apply(null, new Array(7)).map(Number.prototype.valueOf,0);
+      var lastAlerts = Array.apply(null, new Array(7)).map(Number.prototype.valueOf,0);//7 zeros array
       var today = new moment().set({'seconds': 59, 'minutes': 59, 'hour': 23})
         for (var i = 0; i < user['alerts'].length; i++){
           var day = new moment(user['alerts'][i]['fecha'])
@@ -26,13 +37,25 @@ function dashboardFactory(sharedData){
         }
       return lastAlerts.slice(0, 7).reverse();
     },
-    lastAlertsFindUserNum: function(userName, users){
-      for (var i = 0; i < users.length; i++){
-        if (users[i].userId === userName){
-          return this.lastAlertsUserNum(users[i]);
+    /**
+     * Obtains the last 7 days alerts for an user and discards alerts from other users
+     * @param  {String} userName  
+     * @param  {Array} alerts Alerts from diferent users
+     * @return {Array} 7 elements (sorted by 7 days)
+     */
+    lastAlertsFindUserNum: function(userName, alerts){
+      for (var i = 0; i < alerts.length; i++){
+        if (alerts[i].userId === userName){
+          return this.lastAlertsUserNum(alerts[i]);
         }
       }
     },
+    /**
+     * Obtains an object with user names and corresponding alerts ready to use in a charjs chart
+     * @param  {Array} groups A list of group(people) objects
+     * @param  {Array} alerts A list of alerts from diferent users
+     * @return {Object} usernames and alerts
+     */
     allUsersAlertsNum: function(groups, alerts){
       var chartData = {
         names: [],
@@ -50,7 +73,12 @@ function dashboardFactory(sharedData){
 
       return chartData;
     },
-
+    /**
+     * Counts the number of alerts from a given type
+     * @param  {String} type   
+     * @param  {Array} alerts List of alerts
+     * @return {Integer} 
+     */
     alertsNumberByType: function(type, alerts){
       var alertsCounter = 0;
       for (var i = 0; i < alerts.length; i++){
@@ -60,6 +88,11 @@ function dashboardFactory(sharedData){
       }
       return alertsCounter;
     },
+    /**
+     * Obtains an object with alert types and the number of alerts for this types ready to use in a chartjs chart
+     * @param  {Array} alerts List of alerts
+     * @return {Object}        alertTypes and corresponding alerts
+     */
     allUserAlertsNum: function(alerts){
       var alertsNames = sharedData.getAlertsNames();
 
--- app/assets/javascripts/app/services/maps.factory.js
+++ app/assets/javascripts/app/services/maps.factory.js
@@ -1,14 +1,28 @@
-/* Google Maps Helper functions Factory*/
-
-function mapsFactory(){
+/**
+ * Google Maps Helper functions Factory
+ * @namespace mapsFactory
+ * @memberOf Factories
+ */
+(function mapsFactory(){
   return{
-    //Function to convert hex format to a rgb color
+    /**
+    * @name rgb2hex
+    * @desc Function to convert hex format to a rgb color
+    * @param {String} rgb Rgb number to convert
+    * @returns {String} hexadecimal number
+    * @memberOf Factories.mapsFactory
+    */
     rgb2hex: function (rgb) {
         rgb = Array.apply(null, arguments).join().match(/\d+/g);
         rgb = ((rgb[0] << 16) + (rgb[1] << 8) + (+rgb[2])).toString(16);
 
         return "#" + rgb;
     },
+    /**
+     * @desc Function to obtain a Marker icon to Google Maps with a selected color
+     * @param  {String} pinColor Color to obtain the icon
+     * @return {Array} First element is the icon and second is the shadow
+     */
     pinPoints: function(pinColor){
         var pinImage = new google.maps.MarkerImage("http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|" + pinColor.slice(1,pinColor.length),
             new google.maps.Size(21, 34),
@@ -21,6 +35,14 @@ function mapsFactory(){
 
         return [pinImage, pinShadow]
     },
+    /**
+     * @desc Obtains a Marker to print in a map from Google Maps
+     * @param  {String} markerId 
+     * @param  {Object} payload
+     * @param  {String} color 
+     * @param  {String} userId
+     * @return {Object} Marker formated
+     */
     formatPayloadMapMarker: function(markerId, payload, color, userId){
       var markerPosition = this.parsePosition(payload.data.location)
       var marker = {
@@ -40,16 +62,32 @@ function mapsFactory(){
       }
       return marker;
     },
+    /**
+     * @desc Obtains markers for google maps from some given payloads
+     * @param  {Array} payloads
+     * @param  {Array} colors
+     * @return {Array} A list of markers
+     */
     getAllMarkers: function(payloads, colors){
       var markers = [];
       for (var i = 0; i < payloads.length; i++){
         
         if (payloads[i].payloads[0] != undefined){
-          markers.push(this.formatPayloadMapMarker(i+1, payloads[i].payloads[0], this.rgb2hex(colors[i].fillColor), payloads[i].userId))
+          markers.push(this.formatPayloadMapMarker(i+1, 
+            payloads[i].payloads[0], 
+            this.rgb2hex(colors[i].fillColor), 
+            payloads[i].userId))
         }
       }
       return markers;
     },
+    /**
+     * @desc Obtains a set of markers from some given payloads sorted by user
+     * @param  {Array} payloads 
+     * @param  {Array} colors
+     * @param  {Array} groups A list with objects. Every object include a name String which identify the  user
+     * @return {Array} A list of markers sorted by user
+     */
     getMarkersByUser: function(payloads,colors,groups){
       var usersPayloads = [];
       var users = [];
@@ -65,6 +103,11 @@ function mapsFactory(){
 
       return this.getAllMarkers(usersPayloads, colors)
     },
+    /**
+     * @desc Parse an asset position to a new format
+     * @param  {String} assetPosition
+     * @return {Object} An object with latitude and longitude
+     */
     parsePosition: function(assetPosition){
       var position = assetPosition.split(', ');
       var positionObject = {
@@ -73,6 +116,12 @@ function mapsFactory(){
       }
       return positionObject;
     },
+    /**
+     * @desc Creates a circle object for an specific person (group)
+     * @param  {Object} group
+     * @param  {String} circleId
+     * @return {Object} An circle formated object to use with google Maps
+     */
     parseCircleMapData: function(group, circleId){
       var circle = {
         id: circleId,
@@ -102,6 +151,11 @@ function mapsFactory(){
       }
       return circle;
     },
+    /**
+     * @desc Gets all circles for a group of people
+     * @param  {Array} groups An array with group (People) objects
+     * @return {Array} An array of circle objects ready to use in Google Maps
+     */
     getAllCirclesMapData: function(groups){
       var circles = [];
       for (var i=0; i < groups.length; i++){
@@ -112,4 +166,4 @@ function mapsFactory(){
     }
 
   }
-}
+})();
