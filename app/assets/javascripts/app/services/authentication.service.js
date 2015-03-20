704afb3458a6c21f59b21adc095b5b27c0a02616
Ivan Rodriguez <frivanrodriguez@gmail.com>
2015-03-20 15:08:30 +0100

    Fixing foundation dropdown bug

--- app/assets/javascripts/app/services/authentication.service.js
+++ app/assets/javascripts/app/services/authentication.service.js
@@ -6,6 +6,11 @@
 function authenticationService($q, $timeout, $state){
 	'use strict';
 	return {
+		/**
+		 * @desc Redirects to signIn page if the user is not authenticated
+		 * @param  {Object} authPromise A promise with the Authentication
+		 * @return {Object} A promise
+		 */
 		redirectToSingin: function(authPromise){
 			var deferred = $q.defer();
 			authPromise.then(function(data){
